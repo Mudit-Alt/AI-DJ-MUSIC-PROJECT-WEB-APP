4ea97757b0cf5c2135c190a0b739c625c48f583a
Mudit_Chandak7C <96127224+Mudit-Alt@users.noreply.github.com>
2022-09-09 17:31:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-music_1 = ""
-music_2 = ""
-
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function setup() {
-    canvas = createCanvas(500, 400);
+    canvas = createCanvas(600, 500);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded(){
+    console.log("You Did It!")
+}
+
+function gotPoses(results) {
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
 }
 
 function draw() {
